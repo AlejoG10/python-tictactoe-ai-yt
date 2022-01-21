3a64c1a5a65eabce2ecf683a3809de7ad8d45197
AlejoG10 <alejogonza99@hotmail.com>
2022-01-20 20:59:10 -0500

    Initial Commit

---
+++ constants.py
@@ -0,0 +1,27 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (28, 170, 156)
+LINE_COLOR = (23, 145, 135)
+CIRC_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
